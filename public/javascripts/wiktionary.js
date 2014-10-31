689096f11cbd130b45ef833692bf30a18e0718a5
Adrien BAK <adrien.bak@gmail.com>
2014-10-31 12:42:19 +0900

    query only the relevant section

--- public/javascripts/wiktionary.js
+++ public/javascripts/wiktionary.js
@@ -1,15 +1,17 @@
 var wiki = (function(){
+  var kanji = "";
   var base_url = "http://en.wiktionary.org/w/api.php?";
   var user_agent = "Daily Kanji wiktionary integration (adrien.bak@gmail.com)";
-  var queryParams = {
+  var commonQueryParams = {
     "format"    : "json",
     "action"    : "parse",
-    "prop"      : "text",
     "callback"  : "?"
   };
 
-  function buildQuery(kanji){
+  function buildQuery(queryParams){
     var query = base_url;
+    $.extend(queryParams, commonQueryParams);
+
     for(var key in queryParams){
       if (queryParams.hasOwnProperty(key)){
         query+=key +"="+queryParams[key]+"&";
@@ -19,12 +21,41 @@ var wiki = (function(){
     return query;
   }
 
-  function getKanji(kanji){
-    var query = buildQuery(kanji);
+  function retrieveRelevantSection(section){
+    var queryParams = {
+      "prop"    : "text",
+      "section" : section.index
+    };
 
+    var query = buildQuery(queryParams);
     var result = $.getJSON(query, function(data){
+      console.log(data);
       $("#main").append(data.parse.text["*"]);
+      });
+
+  }
+
+  function queryRelevantSection(){
+    queryParams = {
+      "prop" : "sections"
+    };
+
+    var query = buildQuery(queryParams);
+    var result = $.getJSON(query, function(data){
+      var sections = data.parse.sections.filter(
+        function(i){
+          return i.anchor === "Japanese";
+      });
+      console.log(sections[0]);
+      if(sections.length === 1){
+        retrieveRelevantSection(sections[0]);
+      }
     });
+  }
+
+  function getKanji(item){
+    kanji = item;
+    queryRelevantSection(kanji);
   }
 
   return {
